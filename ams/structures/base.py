c8d72eb1993ed62963dba3445d5327aff6ab1c49
hiro-ya-iv <hiro-ya-iv@gmail.com>
2018-01-19 00:06:39 +0900

    add base getter and AttrDict.

--- ams/structures/base.py
+++ ams/structures/base.py
@@ -5,15 +5,44 @@ from copy import deepcopy
 from ams import Validator
 
 
+class AttrDict(dict):
+    def __getattr__(self, name):
+        if name in self:
+            return self[name]
+        else:
+            raise AttributeError("No such attribute: " + name)
+
+    def __setattr__(self, name, value):
+        self[name] = value
+
+    def __delattr__(self, name):
+        if name in self:
+            del self[name]
+        else:
+            raise AttributeError("No such attribute: " + name)
+
+
 def get_base_class(template, schema):
+
+    attr_template = AttrDict()
+    for key, value in template.items():
+        attr_template[key] = value
+
     class Base(Validator):
         def __init__(self):
-            self.__template = template
+            self.__template = attr_template
             super().__init__(schema)
 
         @staticmethod
         def get_template():
-            return deepcopy(template)
+            return deepcopy(attr_template)
+
+        @staticmethod
+        def get_data(**kwargs):
+            data = deepcopy(attr_template)
+            for k, v in kwargs.items():
+                data[k] = v
+            return data
 
         @staticmethod
         def get_schema():
