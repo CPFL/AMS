8c07c251ca06c1f369a37189aa31376364fefc8b
hiro-ya-iv <hiro-ya-iv@gmail.com>
2018-01-25 00:59:49 +0900

    fix attrdict.

--- ams/structures/base.py
+++ ams/structures/base.py
@@ -7,26 +7,46 @@ from ams import Validator, AttrDict
 
 def get_base_class(template, schema):
 
+    _template = template
+    if isinstance(template, list):
+        _template = {"list": template}
     attr_template = AttrDict()
-    for key, value in template.items():
+    for key, value in _template.items():
         attr_template[key] = value
 
-    validator = Validator(schema)
+    _schema = schema
+    if isinstance(template, list):
+        _schema = {"list": schema}
+    validator = Validator(_schema)
 
     class Base(Validator):
         def __init__(self):
             self.__template = attr_template
-            super().__init__(schema)
+            super().__init__(_schema)
 
         @staticmethod
         def get_template():
+            if isinstance(template, list):
+                return deepcopy(attr_template.list)
             return deepcopy(attr_template)
 
         @staticmethod
-        def get_data(**kwargs):
-            data = deepcopy(attr_template)
-            for k, v in kwargs.items():
-                data[k] = v
+        def get_data(*args, **kwargs):
+            if isinstance(template, list):
+                data = []
+                for _element in args[0]:
+                    element = AttrDict.set_recursively(_element, deepcopy(attr_template.list[0]))
+                    data.append(element)
+                if not validator.validate({"list": data}):
+                    print(validator.validate_errors())
+                    print(data)
+                    raise ValueError
+            else:
+                data = AttrDict.set_recursively(kwargs, deepcopy(attr_template))
+                if not validator.validate(data):
+                    print(validator.validate_errors())
+                    print(data)
+                    raise ValueError
             return data
 
         @staticmethod
@@ -35,7 +55,10 @@ def get_base_class(template, schema):
 
         @staticmethod
         def check_data(data):
-            return validator.validate(data)
+            _data = data
+            if isinstance(template, list):
+                _data = {"list": data}
+            return validator.validate(_data)
 
         @staticmethod
         def get_errors():
--- ams/utilities/attrdict.py
+++ ams/utilities/attrdict.py
@@ -17,3 +17,22 @@ class AttrDict(dict):
             del self[name]
         else:
             raise AttributeError("No such attribute: " + name)
+
+    @staticmethod
+    def set_recursively(_obj, attr_dict=None):
+        if attr_dict is None:
+            attr_dict = AttrDict()
+        if isinstance(_obj, dict):
+            for k, v in _obj.items():
+                if isinstance(v, (dict, list)):
+                    attr_dict[k] = AttrDict.set_recursively(v)
+                else:
+                    attr_dict[k] = v
+            return attr_dict
+        elif isinstance(_obj, list):
+            _list = []
+            for v in _obj:
+                _list.append(AttrDict.set_recursively(v))
+            return _list
+        else:
+            return _obj
