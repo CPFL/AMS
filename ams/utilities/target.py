0ba3ec18ec899f3516fe6e8eb8e05e4b567e7241
hiro-ya-iv <hiro-ya-iv@gmail.com>
2018-02-06 16:04:17 +0900

    fix utilities.target

--- ams/utilities/target.py
+++ ams/utilities/target.py
@@ -7,11 +7,17 @@ from ams.structures import Target as Structure
 class Target(object):
 
     @staticmethod
-    def new_target(instance):
+    def new_target(node):
+        if isinstance(node, type):
+            _id = None
+            name = node.__name__
+        else:
+            _id = node.event_loop_id,
+            name = node.__class__.__name__
         return Structure.new_data(
-            id=instance.event_loop_id,
-            node=instance.__class__.__name__
+            id=_id,
+            node=name
         )
 
-    validate_target = Structure.validatie_data
+    validate_target = Structure.validate_data
     get_errors = Structure.get_errors
