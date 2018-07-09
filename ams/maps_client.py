a6c03ee6ceb36961f6e0f011d2d309205792c936
hiro-ya-iv <hiro-ya-iv@gmail.com>
2018-07-09 13:12:32 +0900

    refactor maps_client

--- ams/maps_client.py
+++ ams/maps_client.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from ams.maps import Waypoint, Arrow, Route
+from ams.maps import Waypoint, Arrow, Route, MapMatch
 
 
 class MapsClient(object):
@@ -9,14 +9,22 @@ class MapsClient(object):
     Waypoint = Waypoint
     Arrow = Arrow
     Route = Route
+    MapMatch = MapMatch
 
     def __init__(self):
         self.waypoint = Waypoint()
+
         self.arrow = Arrow()
         self.arrow.set_waypoint(self.waypoint)
+
         self.route = Route()
         self.route.set_waypoint(self.waypoint)
         self.route.set_arrow(self.arrow)
+
+        self.map_match = MapMatch()
+        self.map_match.set_waypoint(self.waypoint)
+        self.map_match.set_arrow(self.arrow)
+        self.map_match.set_route(self.route)
 
     def connect(self):
         pass
