831461d2eba54f6d1b366f635bd183087a0ec26e
hiro-ya-iv <hiro-ya-iv@gmail.com>
2018-08-20 15:55:20 +0900

    clean

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-python test/test_arrow.py --arrow_path test/res/arrow.json
-python test/res/test_waypoint.py --waypoint_path test/res/waypoint.json
-python test/res/test_intersection.py --intersection_path test/res/intersection.json
-python test/res/test_converter.py
-python test/res/test_map_match.py
-python test/res/test_rpy.py
