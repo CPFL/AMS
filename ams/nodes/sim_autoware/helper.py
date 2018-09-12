b30b3ded9ea201b0f861fb6cc9c57913dc343eb0
hiro-ya-iv <hiro-ya-iv@gmail.com>
2018-09-12 13:42:19 +0900

    refactor sim_autoware state machine

--- ams/nodes/sim_autoware/helper.py
+++ ams/nodes/sim_autoware/helper.py
@@ -172,3 +172,58 @@ class Helper(BaseHelper):
                     clients, target_roles,
                     status.lane_waypoints_array.lanes[0].waypoints[status.closest_waypoint.data].pose)
         return False
+
+
+class Hook(object):
+
+    CONST = CONST
+    Helper = Helper
+
+    @classmethod
+    def initialize_closest_waypoint(cls, clients, target_roles):
+        cls.Helper.initialize_closest_waypoint(clients, target_roles)
+
+    @classmethod
+    def initialize_state_cmd(cls, clients, target_roles):
+        cls.Helper.initialize_state_cmd(clients, target_roles)
+
+    @classmethod
+    def initialize_lane_waypoints_array(cls, clients, target_roles):
+        cls.Helper.initialize_lane_waypoints_array(clients, target_roles)
+
+    @classmethod
+    def update_closest_waypoint(cls, clients, target_roles, config, status):
+        cls.Helper.update_closest_waypoint(clients, target_roles, config, status)
+
+    @classmethod
+    def update_current_pose(cls, clients, target_roles, status):
+        cls.Helper.update_current_pose(clients, target_roles, status)
+
+
+class Condition(object):
+
+    CONST = CONST
+
+    @classmethod
+    def lane_waypoints_array_exists(cls, status):
+        return status.lane_waypoints_array is not None
+
+    @classmethod
+    def closest_waypoint_initialized(cls, status):
+        if status.closest_waypoint is None:
+            return False
+        return status.closest_waypoint.data == 0
+
+    @classmethod
+    def state_cmd_is_engage(cls, status):
+        if status.state_cmd is None:
+            return False
+        return status.state_cmd.data == cls.CONST.STATE_CMD.ENGAGE
+
+    @classmethod
+    def state_cmd_initialized(cls, status):
+        return status.state_cmd is None
+
+    @classmethod
+    def closest_waypoint_is_end_point(cls, status):
+        return status.closest_waypoint.data == len(status.lane_waypoints_array.lanes[0].waypoints) - 1
