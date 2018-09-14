ab6ecfad9ba4bbf5640e27a2ce52708104a1eaa4
makoto tokunaga <tokunaga.makoto@tier4.jp>
2018-09-15 01:59:59 +0900

    add username and password for paho mqtt auth

--- ams/clients/pubsub/paho_mqtt.py
+++ ams/clients/pubsub/paho_mqtt.py
@@ -42,6 +42,10 @@ class ArgsSetters(object):
         self.args.connect = copy(locals())
         self.args.connect.pop("self")
 
+    def set_args_of_username_pw_set(self, username, password=None):
+        self.args.username_pw_set = copy(locals())
+        self.args.username_pw_set.pop("self")
+
 
 def set_on_message_and_connect(client, args, subscribers, subscribers_lock):
     def on_message(_client, _userdata, message_data):
@@ -70,6 +74,8 @@ class PubSubClient(ArgsSetters):
         self.__client = None
 
     def subscribe(self, topic, callback, qos=0, user_data=None, structure=None):
+        logger.info("subscribe {}, qos: {}".format(topic, qos))
+
         def wrapped_callback(_client, _user_data, _topic, _payload):
             message = Topic.unserialize(_payload, structure)
             callback(_client, _user_data, _topic, message)
@@ -96,15 +102,23 @@ class PubSubClient(ArgsSetters):
                 logger.warning('connect status {0}'.format(response_code))
 
         self.__client = Client(**self.args.client)
+
         if "tls_set" in self.args.keys():
             self.__client.tls_set(**self.args.tls_set)
+
         if "tls_insecure_set" in self.args.keys():
             self.__client.tls_insecure_set(**self.args.tls_insecure_set)
+
         if "will_set" in self.args.keys():
             self.__client.will_set(**self.args.will_set)
+
+        if "username_pw_set" in self.args.keys():
+            self.__client.username_pw_set(**self.args.username_pw_set)
+
         self.__client.on_connect = on_connect
         set_on_message_and_connect(
             self.__client, self.args, self.__subscribers, self.__subscribers_lock)
+
         self.__client.loop_start()
 
     def publish(self, topic, message, qos=0, retain=False, wait=False):
