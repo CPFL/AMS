2e14f1fca9de1ae2c8d008a54f2ff3c932012502
hiro-ya-iv <hiro-ya-iv@gmail.com>
2018-10-24 20:24:27 +0900

    rename and add test script

--- tests/arrow.py
+++ tests/arrow.py
@@ -6,11 +6,11 @@ import unittest
 from ams.helpers import Arrow, Position
 
 
-class TestArrow(unittest.TestCase):
+class Test(unittest.TestCase):
 
     def __init__(self, method_name):
 
-        super(TestArrow, self).__init__(method_name)
+        super(Test, self).__init__(method_name)
 
     def test_get_distance(self):
         position1 = Position.new_position(1.0, 2.0, 3.0)
@@ -24,21 +24,3 @@ class TestArrow(unittest.TestCase):
         value = Arrow.split_arrow_code(arrow_code)
         expected = ["0", "1"]
         self.assertEqual(expected, value)
-
-
-def test_arrow(test_loader, suite):
-    test_names = test_loader.getTestCaseNames(TestArrow)
-
-    for test_name in test_names:
-        suite.addTest(TestArrow(test_name))
-
-
-if __name__ == "__main__":
-
-    test_loader = unittest.TestLoader()
-    suite = unittest.TestSuite()
-
-    test_arrow(test_loader, suite)
-
-    result = unittest.TextTestRunner().run(suite)
-    sys.exit(not result.wasSuccessful())
