f8ac67cfdbaec15b15e951499924ba6ac4409774
hiro-ya-iv <hiro-ya-iv@gmail.com>
2018-12-03 19:27:40 +0900

    add end processing to vehicle node

--- ams/nodes/vehicle.py
+++ ams/nodes/vehicle.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from ams.helpers import Subscriber
+from time import sleep
+
+from ams.helpers import Subscriber, Hook
 from ams.structures import Vehicle as Structure
 from ams.structures import AutowareInterface, Autoware, Dispatcher
 from ams.nodes.event_loop import EventLoop
@@ -60,3 +62,14 @@ class Vehicle(EventLoop):
             "structure": Autoware.ROSMessage.DecisionMakerState,
             "user_data": self.user_data
         }
+
+    def loop(self):
+        while True:
+            status = Hook.get_status(self.user_data["kvs_client"], self.user_data["target_vehicle"], self.Status)
+            if status is not None and status.state == Structure.CONST.STATE.END:
+                break
+            sleep(self.dt)
+        if self.user_data["kvs_client"] is not None:
+            self.user_data["kvs_client"].disconnect()
+        if self.user_data["pubsub_client"] is not None:
+            self.user_data["pubsub_client"].disconnect()
