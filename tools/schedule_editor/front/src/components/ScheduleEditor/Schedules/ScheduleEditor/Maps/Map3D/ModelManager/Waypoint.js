8eaf25e7f689a9d5ee94e3a0610df50625ad310a
yabuta <tefutefu1989@gmail.com>
2018-12-24 17:47:49 +0900

    Fix how to display next route in add schedule

--- tools/schedule_editor/front/src/components/ScheduleEditor/Schedules/ScheduleEditor/Maps/Map3D/ModelManager/Waypoint.js
+++ tools/schedule_editor/front/src/components/ScheduleEditor/Schedules/ScheduleEditor/Maps/Map3D/ModelManager/Waypoint.js
@@ -302,6 +302,7 @@ export default class Waypoint extends THREE.Group {
       for (const routeCode of this.routeCodeList) {
         const routeStartPoint = routeCode.startPoint;
         if (endPoint === routeStartPoint) {
+          /*
           this.waypointsList[routeCode.startPoint].material.color.set(
             this.color.selectCandidate
           );
@@ -310,6 +311,14 @@ export default class Waypoint extends THREE.Group {
               this.color.selectCandidate
             );
           }
+          */
+          routeCode.laneList.forEach((laneID, index) => {
+            if (index > 0) {
+              this.laneList[laneID].material.color.set(
+                this.color.selectCandidate
+              );
+            }
+          });
           this.waypointsList[routeCode.endPoint].material.color.set(
             this.color.selectCandidate
           );
